36fbef7439c0ff2f393eb0a340363ad42addae27
saintkim12 <saintkim1232@gmail.com>
2022-01-25 13:25:23 +0900

    eslint 초기화 및 useref 테스트중

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'preact'],
+  // plugins: ['eslint-plugin-html', 'eslint-plugin-react'],
+  plugins: ['eslint-plugin-html'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'jest/no-deprecated-functions': false,
+  },
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'preact/hooks'
+import { useEffect, useRef } from 'preact/hooks'
 import { Logo } from './logo'
 // import alphaTab from '@coderline/alphatab'
 
 export function App() {
+  const wrapper = useRef(null)
+  const api = useRef(null)
+  const [tex, setTex = useRef(null)
   useEffect(() => {
     /** @type {import('@coderline/alphatab')} */
     const alphaTab = window.alphaTab
@@ -11,7 +14,46 @@ export function App() {
     // require(['@coderline/alphatab'], (alphaTab: any) => {
     // console.log('alphaTab', alphaTab)
     // })
+    console.log('wrapper', wrapper.current)
+    api.current = new alphaTab.AlphaTabApi(wrapper.current)
+    api.current.tex(`
+      \\title "hello"
+      .
+    `)
   })
+  useEffect(() => {
+    if (tex.current) {
+      api.current.tex(tex.current)
+    }
+    // api.tex(`
+    //   \\title "hello"
+    //   .
+    //   \\track "Piano"
+    //     \\staff
+    //     \\tuning piano
+    //     \\instrument  acousticgrandpiano
+    //     c4 d4 e4 f4
+    //     \\staff
+    //     \\clef F4 // grand-staff F4 clef
+    //     c2 c2 c2 c2 
+    // `)
+  }, [tex])
+  const onClicked = () => {
+    console.log('onClicked')
+    tex.current = `
+      \\title "hello"
+      .
+      \\track "Piano"
+        \\staff
+        \\tuning piano
+        \\instrument  acousticgrandpiano
+        c4 d4 e4 f4
+        \\staff
+        \\clef F4 // grand-staff F4 clef
+        c2 c2 c2 c2 
+    `
+    console.log('tex.current', tex.current)
+  }
   return (
     <>
       <Logo />
@@ -26,6 +68,9 @@ export function App() {
           Learn Preact
         </a>
       </p>
+      <div>{test.current}</div>
+      <button type="button" onClick={onClicked}>button</button>
+      <div ref={wrapper} id="test" data-tex="true"></div>
     </>
   )
 }
